ab63f6eadd7408c85a8c62f40e19dba1eee8e8b9
聂升 <sheng.nie@transsion.com>
2025-04-28 17:56:03 +0800

    feat: clear gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,17 @@ import type { NextConfig } from "next";
 import withPWA, { PWAConfig } from "next-pwa";
 
 // const isVercel = !!process.env.VERCEL; // Vercel 平台会自动注入
-const isGitHubPages = process.env.DEPLOY_ENV === "GH_PAGES"; // 我们在 Action 里设置
+// const isGitHubPages = process.env.DEPLOY_ENV === "GH_PAGES"; // 我们在 Action 里设置
 
-// 如果是 GH_PAGES，则给出静态导出相关配置
-const staticExportConfig: Partial<NextConfig> = isGitHubPages
-  ? {
-      output: "export",
-      trailingSlash: true,
-      basePath: "/FrameAI",
-      assetPrefix: "/FrameAI",
-    }
-  : {};
+// // 如果是 GH_PAGES，则给出静态导出相关配置
+// const staticExportConfig: Partial<NextConfig> = isGitHubPages
+//   ? {
+//       output: "export",
+//       trailingSlash: true,
+//       basePath: "/FrameAI",
+//       assetPrefix: "/FrameAI",
+//     }
+//   : {};
 
 // 1. 定义 PWA 插件的配置
 const pwaOptions: PWAConfig = {
@@ -41,7 +41,6 @@ const withPWAConfig = withPWA(pwaOptions);
 // 3. 定义纯 Next.js 配置
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  ...staticExportConfig,
 };
 
 // 4. 导出最终配置
