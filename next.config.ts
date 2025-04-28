1a01a6a2797b377aa662616bf28b56d140e93c6f
聂升 <sheng.nie@transsion.com>
2025-04-28 14:35:58 +0800

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  exportTrailingSlash: true, // 如果你需要 /page/ 类型的 URL
-  // reactStrictMode: true,
+  reactStrictMode: true, // 可选：开启严格模式以帮助开发
+  trailingSlash: true, // 配置 Next.js 输出的路径末尾是否加上斜杠
   // 其他 Next.js 配置项
 };
 
@@ -12,4 +12,17 @@ export default withPWA({
   dest: "public", // 设置 Service Worker 存放的位置
   register: true, // 自动注册 Service Worker
   skipWaiting: true, // 跳过等待阶段
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "my-cache",
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 * 24, // 1 天的缓存
+        },
+      },
+    },
+  ],
 });
